2484d04983ffb3506da67e31948186254eb5e9e8
Ryan Skotnicki <ryanskotnicki@gmail.com>
2020-05-14 17:21:41 -0600

    Complete - used to test express-crud-api

---
+++ index.js
@@ -0,0 +1,110 @@
+const axios = require('axios');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const qs = require('querystring');
+
+dotenv.config();
+
+const baseUrl = 'http://localhost:3000/';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.set('port', (process.env.PORT || 8000));
+
+app.get('/api/goals', async (req, res, next) => {
+  try {
+    const url = `${baseUrl}goals`;
+    const axiosConfig = {
+      timeout: 5000
+    };
+    const result = await axios.get(url, axiosConfig);
+    console.log(result.data);
+    res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+app.get('/api/goals/:id', async (req, res, next) => {
+  try {
+    const url = `${baseUrl}goals/${req.params.id}`;
+    const axiosConfig = {
+      timeout: 5000
+    };
+    const result = await axios.get(url, axiosConfig);
+    console.log(result.data);
+    res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+app.delete('/api/goals/:id', async (req, res, next) => {
+  try {
+    const url = `${baseUrl}goals/${req.params.id}`;
+    const axiosConfig = {
+      timeout: 5000
+    };
+    const result = await axios.delete(url, axiosConfig);
+    console.log(result.data);
+    res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+app.put('/api/goals/:id', async (req, res, next) => {
+  try {
+    const url = `${baseUrl}goals/${req.params.id}`;
+    const params = {
+      'title': 'An updated goal',
+      'description': 'This describes the new goal',
+      'complete': true
+    };
+    const axiosConfig = {
+      timeout: 5000
+    };
+    const result = await axios.put(url, qs.stringify(params), axiosConfig);
+    console.log(result.data);
+    res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+app.post('/api/goals', async (req, res, next) => {
+  try {
+    const url = `${baseUrl}goals`;
+    const params = {
+      'title': 'A new goal',
+      'description': 'This describes the new goal',
+      'complete': false
+    };
+    const axiosConfig = {
+      timeout: 5000
+    };
+    const result = await axios.post(url, qs.stringify(params), axiosConfig);
+    console.log(result.data);
+    res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server is now running on port ${app.get('port')}!`);
+});
